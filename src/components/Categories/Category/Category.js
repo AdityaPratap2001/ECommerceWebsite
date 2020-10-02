ca077e067606d3e508991311abfc1c0ff2d74640
Aditya <pratapsinghaditya2001@gmail.com>
2020-10-02 15:23:12 +0530

    Removed minor bug in categorySection

--- src/components/Categories/Category/Category.js
+++ src/components/Categories/Category/Category.js
@@ -5,10 +5,10 @@ const Category = (props) => {
   
   let className = 'category '
 
-  if(props.type === 'home&living'){
+  if(props.type === 'Home_&_Living'){
     className += 'shift_right';
   }
-  else if(props.type === 'travel_bags'){
+  else if(props.type === 'Travel_Bags'){
     className += 'shift_left';
   }
 
